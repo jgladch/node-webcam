80ed339d112a5d855780b279d0b2a86c976a753c
jgladch <jeffgladchun@gmail.com>
2016-10-04 13:15:51 -0400

    Saturation 18%

--- src/webcams/FSWebcam.js
+++ src/webcams/FSWebcam.js
@@ -22,7 +22,7 @@ function FSWebcam( options ) {
     scope.opts = Utils.setDefaults( options, FSWebcam.Defaults );
 
     scope.opts.rotate = '180';
-    scope.opts.set = 'Saturation:12%';
+    scope.opts.set = 'Saturation:18%';
 
     Webcam.call( scope, scope.opts );
 
